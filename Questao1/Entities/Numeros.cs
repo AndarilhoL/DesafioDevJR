bacf2628a85849d7b24021b1f044bd0393061121
AndarilhoL <leonardosantos2742@gmail.com>
2021-02-12 20:30:02 -0300

    Subindo estrutura de pastas

---
+++ Questao1/Entities/Numeros.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Questao1.Entities
+{
+    public static class Numeros
+    {
+        public static int[] ArrayNumeros { get; set; }
+
+        public static int[] RetornarArrayPadrao()
+        {
+            int[] numerosArray = { 2, 7, 11, 15 };
+            ArrayNumeros = numerosArray;
+
+            return numerosArray;
+        }   
+        
+        public static int[] RetornarArrayDinamico(int tamanho)
+        {
+            Random numeroRandom = new Random();
+            int[] numerosArray = new int[4];
+
+            for (int i = 0; i < tamanho; i++)
+            {
+                numerosArray.SetValue(numeroRandom.Next(1, 100), i);
+            }
+
+            ArrayNumeros = numerosArray;
+
+            return numerosArray;
+        }
+
+        public static int[] RetornarIndices(int alvo)
+        {
+            int[] arrayDeIncices = new int[2];
+
+            for (int i = 0; i <= ArrayNumeros.Length-1; i++)
+            {
+                int valorInicial = (int)ArrayNumeros.GetValue(i);
+
+                for (int index = i; index <= ArrayNumeros.Length-1; index++)
+                {
+                    int valor = (int)ArrayNumeros.GetValue(index);
+
+                    if ((valorInicial+valor) == alvo)
+                    {
+                        arrayDeIncices.SetValue(i,0);
+                        arrayDeIncices.SetValue(index,1);
+
+                        return arrayDeIncices;
+                    }           
+                }
+            }
+
+            return arrayDeIncices;
+        }
+    }
+}
---
+++ Questao1/Program.cs
@@ -0,0 +1,65 @@
+﻿using Questao1.Entities;
+using System;
+
+namespace Questao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string resposta;
+
+            Console.WriteLine("QUESTÃO 1");
+            Console.WriteLine("");
+            Console.WriteLine("Digite '1' para usar o Array Padrão (Valores setados)");
+            Console.WriteLine("Digite '2' para usar o Array Dinâmico (Valores dinâmicos)");
+
+            Console.Write("Digite a opção desejada: ");
+            resposta = Console.ReadLine();
+
+            while (resposta != "1" && resposta != "2")
+            {
+                Console.WriteLine("Valor inválido");
+                Console.WriteLine("Digite a opção entre 1 e 2");
+                resposta = Console.ReadLine();
+            }
+
+            Console.WriteLine("============================================================");
+            Console.WriteLine("");
+
+            if (resposta == "1")
+            {
+                var arrayNumeros = Numeros.RetornarArrayPadrao();
+                Array.ForEach(arrayNumeros, numeros => Console.Write($"{numeros} "));
+            }
+            else
+            {
+                Console.Write("Digite o tamanho desejado para o Array (quanto números poderão haver dentro dele):");
+                int tamanho = int.Parse(Console.ReadLine());
+                var arrayNumeros = Numeros.RetornarArrayDinamico(tamanho);
+
+                Array.ForEach(arrayNumeros, numeros => Console.Write($"{numeros} "));
+            }
+
+            Console.WriteLine("");
+            Console.Write("Digite um valor ALVO, esse valor deve ser a soma de dois valores entre os que estão no Array:");
+            int alvo = int.Parse(Console.ReadLine());
+            var indicesRetornados = Numeros.RetornarIndices(alvo);
+
+            Console.WriteLine("");
+            Console.WriteLine("============================================================");
+            if (indicesRetornados[0] == 0 && indicesRetornados[1] == 0)
+            {
+                Console.WriteLine("O valor ALVO não é a soma de nenhum dos valores no Array :(");
+            }
+            else
+            {
+                Console.WriteLine("Esses são os índices da soma desejada :");
+                Array.ForEach(indicesRetornados, indices => Console.Write($"{indices} "));
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("============================================================");
+            }           
+        }
+    }
+}
