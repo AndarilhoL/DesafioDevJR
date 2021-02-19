5b949e5e2d9d3eda0c12eb46dadcc15a0e760549
AndarilhoL <leonardosantos2742@gmail.com>
2021-02-19 14:09:37 -0300

    subindo parte do front da aplicacao, alguns testes de component

---
+++ DesafioAPI/Data/DesafioDataContext.cs
@@ -0,0 +1,15 @@
+﻿using DesafioAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesafioAPI.Data
+{
+    public class DesafioDataContext : DbContext 
+    {
+        public DesafioDataContext(DbContextOptions<DesafioDataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
